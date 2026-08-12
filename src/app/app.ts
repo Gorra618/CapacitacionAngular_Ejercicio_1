90710182f4b6f8f495dadff1bc786b5731a75b85
Gorra618 <messier57lyr@gmail.com>
2026-08-12 11:11:27 -0300

    Version: 0

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataForm } from './data-form/data-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DataForm],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Ejercicio_1');
